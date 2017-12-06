a8fcd3d825a8c1f9e8607b9c1d96b3cbb96d46fc
soulgalore <peter@soulgalore.com>
2017-12-06 15:34:13 +0100

    use statsHelpers from context

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
-const messageMaker = require('sitespeed.io/lib/support/messageMaker');
+const messageMaker = require("sitespeed.io/lib/support/messageMaker");
 
-const analyzer = require('./src/analyzer');
-const aggregator = require('./src/aggregator');
+const analyzer = require("./src/analyzer");
+const aggregator = require("./src/aggregator");
 
 // We need to prefix the plugin name with 'browsertime' to force
 // lib/plugins/graphite to add connectivity
 // https://github.com/sitespeedio/sitespeed.io/blob/master/lib/plugins/graphite/data-generator.js#L12
-const pluginName = 'browsertimeChrometrace';
+const pluginName = "browsertimeChrometrace";
 const make = messageMaker(pluginName).make;
 
 let runIndex = {};
 
-function processChrometraceMessage(message, queue, options) {
-    const { url, group, data } = message;
-    const results = analyzer(data);
+function processChrometraceMessage(message, queue, statsHelpers, options) {
+  const { url, group, data } = message;
+  const results = analyzer(data);
 
-    aggregator.addToAggregate(results, url);
+  aggregator.addToAggregate(results, url, statsHelpers);
 
-    if (typeof runIndex[url] === 'undefined') {
-        runIndex[url] = 0;
-    }
+  if (typeof runIndex[url] === "undefined") {
+    runIndex[url] = 0;
+  }
 
+  queue.postMessage(
+    make(
+      `${pluginName}.run`,
+      {
+        runIndex: runIndex[url],
+        data: results
+      },
+      {
+        url,
+        group,
+        runIndex
+      }
+    )
+  );
+
+  runIndex[url] = runIndex[url] + 1;
+
+  // Trigger pageSummary for each last run
+  if (runIndex[url] === options.iterations) {
     queue.postMessage(
-        make(
-            `${pluginName}.run`,
-            {
-                runIndex: runIndex[url],
-                data: results
-            },
-            {
-                url,
-                group,
-                runIndex
-            }
-        )
+      make(
+        `${pluginName}.pageSummary`,
+        aggregator.getSummary(url, statsHelpers),
+        {
+          url,
+          group
+        }
+      )
     );
-
-    runIndex[url] = runIndex[url] + 1;
-
-    // Trigger pageSummary for each last run
-    if (runIndex[url] === options.iterations) {
-        queue.postMessage(
-            make(
-                `${pluginName}.pageSummary`,
-                aggregator.getSummary(url),
-                {
-                    url,
-                    group
-                }
-            )
-        );
-    }
+  }
 }
 
 module.exports = {
-    name() {
-        return pluginName
-    },
+  name() {
+    return pluginName;
+  },
 
-    open(context, options) {
-        this.options = options.chrometrace || {};
-        this.options.iterations = options.browsertime.iterations;
-    },
+  open(context, options) {
+    this.options = options.chrometrace || {};
+    this.options.iterations = options.browsertime.iterations;
+    this.statsHelpers = context.statsHelpers;
+  },
 
-    processMessage(message, queue) {
-        if (message.type !== 'browsertime.chrometrace') {
-            return;
-        }
-
-        return processChrometraceMessage(message, queue, this.options);
+  processMessage(message, queue) {
+    if (message.type !== "browsertime.chrometrace") {
+      return;
     }
+
+    return processChrometraceMessage(
+      message,
+      queue,
+      this.statsHelpers,
+      this.options
+    );
+  }
 };
--- src/aggregator.js
+++ src/aggregator.js
@@ -1,20 +1,21 @@
-const forEach = require('lodash.foreach');
-const statsHelpers = require('sitespeed.io/lib/support/statsHelpers');
+const forEach = require("lodash.foreach");
 
-let aggregated = {}
-let grouped = {}
-let summary = {}
+let aggregated = {};
+let grouped = {};
+let summary = {};
 
 function walkStats(timelineRun, cb) {
   forEach(timelineRun, (groups, typeName) => {
     forEach(groups, (groupMetrics, groupName) => {
-      forEach(groupMetrics, (value, name) => cb(typeName, groupName, name, value));
+      forEach(groupMetrics, (value, name) =>
+        cb(typeName, groupName, name, value)
+      );
     });
   });
 }
 
-function addToAggregate(timelineRun, url) {
-  if (typeof aggregated[url] === 'undefined') {
+function addToAggregate(timelineRun, url, statsHelpers) {
+  if (typeof aggregated[url] === "undefined") {
     aggregated[url] = {};
   }
 
@@ -24,18 +25,14 @@ function addToAggregate(timelineRun, url) {
       grouped,
       [typeName, groupName, name],
       value
-    )
+    );
   });
 }
 
-function getSummary(url) {
+function getSummary(url, statsHelpers) {
   walkStats(aggregated[url], (typeName, groupName, name, value) =>
-    statsHelpers.setStatsSummary(
-      summary,
-      [typeName, groupName, name],
-      value
-    )
-  )
+    statsHelpers.setStatsSummary(summary, [typeName, groupName, name], value)
+  );
 
   return summary;
 }
